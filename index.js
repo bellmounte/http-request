e690673364070e1518bad85cc2dbc7ce59123363
Ryan D'Acquisto <ryandacquisto@gmail.com>
2016-12-12 07:15:47 -0800

    Add support for following redirects.

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
 const http = require('http');
 const https = require('https');
+const url = require('url');
+
+const overwriteKeys = [
+    'protocol',
+    'auth',
+    'host',
+    'port',
+    'hostname',
+    'path'
+];
 
 const httpRequest = (options, data) => {
 	return new Promise((resolve, reject) => {
-		const request = http.request(options, (response) => {
+        const callback = (response) => {
 			const body = [];
+			const followRedirect = (response.statusCode === 301 && options.followAllRedirects);
 
 			if (response.statusCode < 200 || response.statusCode > 299) {
-				reject(new Error('Failed to load page, status code: ' + response.statusCode));
+                if (followRedirect) {
+                    const redirectURL = url.parse(response.headers.location);
+                    overwriteKeys.forEach(key => {
+                        if (typeof redirectURL[key] !== 'undefined') {
+                            options[key] = redirectURL[key];
+                        }
+                    });
+
+                    httpRequest(options, data).then(data => {
+                        resolve(data);
+                    }).catch(error => {
+                        reject(error);
+                    })
+
+                } else {
+	                reject(new Error('Failed to load page, status code: ' + response.statusCode));
+                }
 			}
 
 			response.on('data', (chunk) => body.push(chunk));
-			response.on('end', () => resolve(body.join('')));
-		});
+			response.on('end', () => {
+                if (!followRedirect) {
+                    resolve(body.join(''));
+                }
+            });
+		};
 
-		let request;
+        let request;
         if (options.protocol === 'https:') {
             request = https.request(options, callback);
         } else {
